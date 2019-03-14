391cd0771b3658370d8f674149fef988894f041b
Wiput Pootong <wiput.pootong@gmail.com>
2019-03-15 02:28:04 +0700

    :arrow_up: update dependencies

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/wiput/Website/itcamp15-registration-frontend/node_modules/react-static-plugin-emotion",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/wiput/Website/itcamp15-registration-frontend/node_modules/react-static-plugin-typescript",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/wiput/Website/itcamp15-registration-frontend/node_modules/react-static-plugin-sass",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/wiput/Website/itcamp15-registration-frontend",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
